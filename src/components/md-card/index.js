c56db7a13f82dac8ceeffa419766962b4a1bed18
Nicholas Gustafson <nichgus7@gmail.com>
2019-12-13 17:53:42 -0500

    RUK-12: Adding medium card to the home page

---
+++ src/components/md-card/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './medium-card.scss';
+
+export const MediumCard = ({
+  bigText,
+  backgroundImageUrl="https://res.cloudinary.com/dnglix0tu/image/upload/v1576275464/bike%20registration/make-your-event.jpg"
+}) => (
+  <div className="md_card" style={{ backgroundImage: `url(${backgroundImageUrl})`}}>
+    <div className="md_card__shade">
+      <h3 className="md_card__text">{bigText}</h3>
+    </div>
+  </div>
+);
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { OurNavBar } from '../../components/navbar';
 import { BigCard } from '../../components/big-card';
+import { MediumCard } from '../../components/md-card';
 
 import './home.scss';
 
@@ -9,6 +10,11 @@ export const Home = () => (
     <OurNavBar />
     <div className="home_card_layout">
       <BigCard bigText="Find Your Race" />
+      <MediumCard bigText="Create Your Race" />
+      <MediumCard
+        bigText="View Your Results"
+        backgroundImageUrl="https://res.cloudinary.com/dnglix0tu/image/upload/v1576277090/bike%20registration/see-your-results.jpg"
+      />
     </div>
   </>
 );
